49e94e27eb70751456f03366ee224f5aec79e2f6
Theo van der Donk <theo.van.der.donk@opinity.nl>
2023-12-20 13:13:38 +0100

    Added check docu

--- core/checks/checks.go
+++ core/checks/checks.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 )
 
+// Checks that `actual` is equal to the `expected` value.
 func Equal(tester *testing.T, expected any, actual any, msg string) {
 	exp, err := json.Marshal(expected)
 	if err != nil {
@@ -23,6 +24,7 @@ func Equal(tester *testing.T, expected any, actual any, msg string) {
 	}
 }
 
+// Checks that `actual` is equal to one of the provided `expected` values.
 func EqualOneOf(tester *testing.T, expected []any, actual any, msg string) {
 	act, err := json.Marshal(actual)
 	if err != nil {
@@ -44,6 +46,7 @@ func EqualOneOf(tester *testing.T, expected []any, actual any, msg string) {
 	tester.Fatalf("FAILED  %s (expected: `%+v`, actual: `%+v`)", msg, expected, actual)
 }
 
+// Checks that `actual` is not nil.
 func IsNotNil(tester *testing.T, actual any, msg string) {
 	if actual != nil {
 		tester.Logf("passed  %s (actual = `%+v` is not nil)", msg, actual)
